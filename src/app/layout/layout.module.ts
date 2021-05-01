b770ef0dc907af87ad616fadf4e558d90e43f7b8
j.orlowski <j.orlowski@oands.com.pl>
2021-05-01 10:12:47 +0200

    sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [FooterComponent, NavbarComponent],
+  declarations: [FooterComponent, NavbarComponent, SidebarComponent],
+  exports: [FooterComponent, NavbarComponent, SidebarComponent],
   imports: [
     CommonModule
   ]
