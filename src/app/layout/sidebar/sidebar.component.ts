a027040997108c8a686f05a4035fd1d241f5a71e
j.orlowski <j.orlowski@oands.com.pl>
2021-05-02 13:47:29 +0200

    sidebar - routing menu

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   items: string[] = ['O mnie', 'Posty', 'Archiwum postów', 'Kontakt'];
 
+
+  route(item): void {
+    if (item == "O mnie") {
+      this.router.navigateByUrl('/home');
+    } else if (item == "Posty") {
+      this.router.navigateByUrl('/posts');
+    } else if (item == "Archiwum postów") {
+      this.router.navigateByUrl('/posts-arch');
+    } else if (item == "Kontakt") {
+      this.router.navigateByUrl('/contact');
+    }
+  }
 }
