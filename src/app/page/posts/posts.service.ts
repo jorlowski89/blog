467409a8d5add40f8a1de6941e3c97841c07b7c4
j.orlowski <j.orlowski@oands.com.pl>
2021-05-02 15:00:50 +0200

    serwis do pobierania listy postów

--- src/app/page/posts/posts.service.ts
+++ src/app/page/posts/posts.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  resourseUrl = 'http://localhost:3000/posts';
 
-  constructor() { }
+  constructor(private httpClien: HttpClient) { }
+
+  getPosts() {
+    return this.httpClien.get(this.resourseUrl).toPromise();
+  }
 }
