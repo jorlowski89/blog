6aaba2a4a3e24f5fd4f23c05099e01ab0f85b17a
j.orlowski <j.orlowski@oands.com.pl>
2021-05-03 10:44:59 +0200

    usuniecie walidacji

--- src/app/page/posts/new-post/new-post.component.ts
+++ src/app/page/posts/new-post/new-post.component.ts
@@ -9,7 +9,6 @@ import { PostsService } from '../posts.service';
 })
 export class NewPostComponent implements OnInit {
   postForm: FormGroup;
-  showErrors: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -18,29 +17,16 @@ export class NewPostComponent implements OnInit {
 
   ngOnInit(): void {
     this.postForm = this.formBuilder.group({
-      title: ['', Validators.minLength(5)],
+      title: '',
       text: '',
     })
   }
 
 
   save(): void {
-    this.showErrors = true;
-
     const formValue = this.postForm.getRawValue();
-    const post = {
-      ...formValue
-    }
-
-    if (this.postForm.valid) {
-      console.log('publikuję post');
-      this.postsService.addPost(post)
-        .then(response => {
-          console.log('Kanapka zapisana', response);
-          this.showErrors = false;
-          // this.reset()
-        })
-    }
+    const post = { ...formValue }
+    this.postsService.addPost(post)
+    this.postsService.getPosts()
   }
-
 }
